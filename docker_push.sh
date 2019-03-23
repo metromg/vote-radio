4997394c8255bea30599a119fbc0664e6a7e0793
Michael Günter <zyborglp@outlook.com>
2019-03-23 20:41:55 +0100

    fix travis deployment

---
+++ docker_push.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker push michaelguenter/radio-backend-external
+docker push michaelguenter/radio-backend-internal
+docker push michaelguenter/radio-backend-console
+docker push michaelguenter/radio-streaming
+docker push michaelguenter/radio-playback
