3080baac7e7ee8e9d2470cb413e7ddb0e9653d24
Michael Günter <zyborglp@outlook.com>
2019-04-03 16:46:16 +0200

    add react app polyfills

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,3 +1,5 @@
+import 'react-app-polyfill/ie11';
+
 import { combineReducers, createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import { localizeReducer } from 'react-localize-redux';
