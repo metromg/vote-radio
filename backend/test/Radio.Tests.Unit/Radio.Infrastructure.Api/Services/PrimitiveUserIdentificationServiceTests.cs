82db2a2cb151acbd7cca33e5faa11aae35461b57
Michael Günter <zyborglp@outlook.com>
2019-04-06 18:55:57 +0200

    backend: add infrastructure api tests

---
+++ backend/test/Radio.Tests.Unit/Radio.Infrastructure.Api/Services/PrimitiveUserIdentificationServiceTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using NSubstitute;
+using NUnit.Framework;
+using Radio.Infrastructure.Api.Services;
+
+namespace Radio.Tests.Unit.Radio.Infrastructure.Api.Services
+{
+    [TestFixture]
+    public class PrimitiveUserIdentificationServiceTests
+    {
+        private IPrimitiveUserIdentificationService _primitiveUserIdentificationService;
+
+        [SetUp]
+        public void Setup()
+        {
+            _primitiveUserIdentificationService = new PrimitiveUserIdentificationService();
+        }
+
+        [Test]
+        public void GetOrCreateUserId_WithExistingUserId_ReturnsExistingUserId()
+        {
+            // Arrange
+            var existingUserId = Guid.NewGuid();
+
+            var httpContext = Substitute.For<HttpContext>();
+            httpContext.Request.Cookies["USER_ID"].Returns(existingUserId.ToString());
+
+            // Act
+            var userId = _primitiveUserIdentificationService.GetOrCreateUserId(httpContext);
+
+            // Assert
+            Assert.That(userId, Is.EqualTo(existingUserId));
+
+            httpContext.Response.Cookies.Received(0).Append("USER_ID", Arg.Any<string>(), Arg.Any<CookieOptions>());
+        }
+
+        [Test]
+        public void GetOrCreateUserId_WithNoExistingUserId_ReturnsNewUserId()
+        {
+            // Arrange
+            var httpContext = Substitute.For<HttpContext>();
+            httpContext.Request.Cookies["USER_ID"].Returns(default(string));
+
+            // Act
+            var userId = _primitiveUserIdentificationService.GetOrCreateUserId(httpContext);
+
+            // Assert
+            Assert.That(userId, Is.Not.Null);
+            Assert.That(userId, Is.Not.EqualTo(Guid.Empty));
+
+            httpContext.Response.Cookies.Received(1).Append("USER_ID", userId.ToString(), Arg.Any<CookieOptions>());
+        }
+    }
+}
