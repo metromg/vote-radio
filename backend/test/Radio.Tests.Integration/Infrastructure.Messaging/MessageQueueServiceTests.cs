9fdce457a8c9c16c21551e48a2762d1c554573ae
Michael Günter <zyborglp@outlook.com>
2019-04-06 21:38:46 +0200

    add message queue service tests

---
+++ backend/test/Radio.Tests.Integration/Infrastructure.Messaging/MessageQueueServiceTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading.Tasks;
+using Autofac;
+using NUnit.Framework;
+using Radio.Core.Services;
+
+namespace Radio.Tests.Integration.Infrastructure.Messaging
+{
+    [TestFixture]
+    public class MessageQueueServiceTests
+    {
+        private IContainer _rootContainer;
+        private IMessageQueueService _messageQueueService;
+
+        [SetUp]
+        public void Setup()
+        {
+            _rootContainer = IntegrationTestHelper.SetUp().Build();
+            _messageQueueService = _rootContainer.Resolve<IMessageQueueService>();
+        }
+
+        [Test]
+        public void MessageQueueService_IsSingleton()
+        {
+            // Arrange
+            var lifeTimeScope1 = _rootContainer.BeginLifetimeScope();
+            var lifeTimeScope2 = _rootContainer.BeginLifetimeScope();
+
+            // Act
+            var messageQueueService1 = lifeTimeScope1.Resolve<IMessageQueueService>();
+            var messageQueueService2 = lifeTimeScope2.Resolve<IMessageQueueService>();
+
+            // Assert
+            Assert.That(messageQueueService1, Is.SameAs(messageQueueService2));
+        }
+
+        [Test]
+        public void MessageQueueService_CanSendAndReceive()
+        {
+            // Arrange & Act
+            var taskCompletionSource = new TaskCompletionSource<Message>();
+            Task.Factory.StartNew(() =>
+            {
+                _messageQueueService.Receive()
+                    .Subscribe(message => taskCompletionSource.SetResult(message));
+
+                _messageQueueService.Send(Message.VotingMessage);
+            });
+
+            var result = taskCompletionSource.Task.Result;
+
+            // Assert
+            Assert.That(result, Is.EqualTo(Message.VotingMessage));
+        }
+    }
+}
