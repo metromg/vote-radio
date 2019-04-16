09ebaa103812111d7699442f6461e6ca47df8613
Michael Günter <zyborglp@outlook.com>
2019-04-16 18:45:13 +0200

    fix docker-entrypoints

--- streaming/docker-entrypoint.sh
+++ streaming/docker-entrypoint.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-mv /etc/icecast2/icecast.xml /etc/icecast2/icecast.xml.mo
-./mo /etc/icecast2/icecast.xml.mo > /etc/icecast2/icecast.xml
-rm /etc/icecast2/icecast.xml.mo
+./mo /etc/icecast2/icecast.mo.xml > /etc/icecast2/icecast.xml
 exec "$@"
